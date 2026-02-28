a2a4220cef94dc16b6e6b885f2eadd7bc3eb8c8b
Ahmed Sibawayh <ahmed712315@gmail.com>
2026-02-28 12:21:02 +0200

    Implement data cleaning and insertion for sales details
    Added logic to clean and insert data into silver.crm_sales_details from bronze.crm_sales_details, including date conversions and calculations for sales and price.

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -8,7 +8,6 @@ INSERT INTO silver.crm_cust_info(
 	cst_martial_status,
 	cst_create_date
 )
-
 SELECT
 cst_id,
 cst_key,
@@ -60,3 +59,45 @@ END AS prd_line,
 CAST(prd_start_dt AS DATE) AS prd_start_date,
 CAST(LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) AS prd_end_dt
 FROM bronze.crm_prd_info
+
+--- cleaning and inserting data into silver.crm_sales_details
+INSERT INTO silver.crm_sales_details (
+	sls_ord_num  ,
+    sls_prd_key  ,
+    sls_cust_id  ,
+    sls_order_dt ,
+    sls_ship_dt  ,
+    sls_due_dt   ,
+    sls_sales    ,
+    sls_quantity ,
+    sls_price
+)
+SELECT
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE 
+	WHEN LEN(sls_order_dt) != 8 OR sls_order_dt = 0 
+	THEN NULL
+	ELSE CONVERT(DATE,CAST(sls_order_dt AS VARCHAR(8)), 112)
+	END AS sls_order_dt,
+CASE 
+	WHEN LEN(sls_ship_dt) != 8 OR sls_ship_dt = 0 
+	THEN NULL
+	ELSE CONVERT(DATE,CAST(sls_ship_dt AS VARCHAR(8)), 112)
+	END AS sls_ship_dt,
+CASE 
+	WHEN LEN(sls_due_dt) != 8 OR sls_due_dt = 0 
+	THEN NULL
+	ELSE CONVERT(DATE,CAST(sls_due_dt AS VARCHAR(8)), 112)
+	END AS sls_due_dt,
+CASE WHEN sls_price IS NULL
+	 THEN sls_sales / NULLIF(sls_quantity,0)
+	 ELSE ABS(sls_price)
+	 END AS sls_price,
+sls_quantity,
+CASE WHEN sls_price IS NULL OR sls_quantity IS NULL
+	 THEN sls_sales
+	 ELSE ABS(sls_price) * sls_quantity
+	 END AS sls_sales
+FROM bronze.crm_sales_details
